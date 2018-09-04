d6d1894e0d097db2b3847a015ce5a1d13b4fb48b
hanqyu <peakjnut37@gmail.com>
2018-09-04 22:42:57 +0900

    change job directory

--- job.py
+++ job.py
@@ -1,8 +1,9 @@
-import scraping_from_naver as sc
-import codes
+import scraping.scraping_from_naver as sc
+import scraping.codes
 
 
-codes = codes.codes()
+codes = scraping.codes.codes()
+
 for corp in codes.itertuples():
     code, name = corp['code'], corp['name']
 
