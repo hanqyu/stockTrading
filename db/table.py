aa91c88fd3986f260fb2962724bdc8181b86cdbc
hanqyu <peakjnut37@gmail.com>
2018-09-12 23:47:16 +0900

    TableSecondary 추가

--- db/table.py
+++ db/table.py
@@ -5,8 +5,8 @@ import sqlite3
 
 class Table:
     def __init__(self, name=None):
-        # import os
-        self.con = sqlite3.connect("/Users/kyujoohan/PycharmProjects/stockTrading/daily.db", timeout=10)
+        import os
+        self.con = sqlite3.connect(os.getcwd() + "/daily.db", timeout=10)
         self.cursor = self.con.cursor()
         self.name = name or 'daily'
         print("SQLITE3: open %s table" % self.name)
@@ -60,3 +60,27 @@ class Table:
 
     def __delete__(self):
         self.close()
+
+
+class TableSecondary(Table):
+    def __init__(self, name):
+        import os
+        self.con = sqlite3.connect(os.getcwd() + "/daily_secondary.db", timeout=10)
+        self.cursor = self.con.cursor()
+        self.name = name
+        print("SQLITE3: open %s table" % self.name)
+
+    def create_table(self, query):
+        self.cursor.execute('PRAGMA encoding="UTF-8"')
+        # query = """CREATE TABLE {tn}(id INTEGER PRIMARY KEY, date date, code char(6), name char(30), open real, high real,
+        #         low real, close real, diff real, volume real)""".format(tn=self.name)
+        try:
+            self.cursor.execute(query)
+        except sqlite3.OperationalError:
+            print("SQLITE3: {tn} already exists".format(tn=self.name))
+
+    def get_columns(self):
+        self.cursor.execute("PRAGMA table_info({tn})".format(tn=self.name))
+
+    def insert_row(self, value, query):
+        self.cursor.execute(query, value)
