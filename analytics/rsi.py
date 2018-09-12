3d16479645bac61d565c7a3e512651aa3ed59a1c
hanqyu <peakjnut37@gmail.com>
2018-09-13 00:28:20 +0900

    rsi를 테이블에 저장하도록 개선
    1. rsi 테이블에 저장(daily_rsi)
    2. 출력되는 csv 형태 개선(volume-price 기준 내림차순)

--- analytics/rsi.py
+++ analytics/rsi.py
@@ -6,7 +6,7 @@ import datetime
 from scraping.codes import codes
 
 
-def RSI(df, period=14):
+def rsi(df, period=14):
     delta = df['close'].diff().dropna()
     u = delta * 0
     d = u.copy()
@@ -21,23 +21,5 @@ def RSI(df, period=14):
     df['rsi'] = rsi
     return df
 
-#
-# result = pd.DataFrame()
-#
-# table = Table()
-# values = ['date', 'code', 'close']
-#
-# for i, name, code in codes().itertuples(name=None):
-#     df = pd.DataFrame(table.get_rows(code, values), columns=values)
-#     RSI(df)
-#
-#     bool = datetime.datetime.today() == table.get_last_date(code).strftime('%Y-%m-%d')  # 오늘일경우
-#     rsi = df[df['date'] == table.get_last_date(code).strftime('%Y-%m-%d %H:%M:%S')]['rsi']
-#     if bool and rsi <= 30:
-#         result['code'] = code
-#         result['name'] = name
-#         result['rsi'] = rsi
-#
-# print(result)
 
 # TODO 벤치마크 측정해서 최적화방향고려하자. 매번 모든 컬럼할수가없음 + RSI 저장해야함
--- job_rsi.py
+++ job_rsi.py
@@ -1,12 +1,12 @@
 #-*- coding: utf-8 -*-
 
 import scraping.codes
-from db.table import Table
-from analytics.rsi import RSI
+from db.table import Table, TableSecondary
+from analytics.rsi import rsi
 from multiprocessing import Pool
-import sys
-import datetime
+from sys import stdout
 import pandas as pd
+import datetime
 
 
 def progressBar(name, value, endvalue, bar_length=20):
@@ -14,14 +14,15 @@ def progressBar(name, value, endvalue, bar_length=20):
     arrow = '=' * int(round(percent * bar_length) - 1) + '>'
     spaces = ' ' * (bar_length - len(arrow))
 
-    sys.stdout.write("\rPercent: [{0}] {1}% - {2} Done".format(arrow + spaces, float(round(percent * 100, 1)), name))
-    sys.stdout.flush()
+    stdout.write("\rPercent: [{0}] {1}% - {2} Done".format(arrow + spaces, float(round(percent * 100, 1)), name))
+    stdout.flush()
 
 
 def get_row_for_rsi(code):
-    values2 = ['date', 'code','name', 'close']
-    df2 = pd.DataFrame(table.get_rows(code, values2), columns=values2)
-    return df2
+    values = ['date', 'code', 'name', 'close', 'volume']
+    df = pd.DataFrame(table_origin.get_rows(code, values), columns=values)
+    df['volume-price'] = df.apply(lambda x: x['close'] * x['volume'])
+    return df, code
 
 
 start_time = datetime.datetime.now()
@@ -29,26 +30,31 @@ print("RSI 계산 시작시간: {0}".format(start_time))
 codes = scraping.codes.codes()
 result = pd.DataFrame(columns=['date', 'code', 'name', 'rsi'])
 
-table = Table()
-values = ['date', 'code', 'name', 'close']
+table_origin = Table()
+table_secondary = TableSecondary("daily_rsi")
 
-with Pool(processes=4) as pool:
-    for df in pool.map(get_row_for_rsi, codes['corp_code'].tolist()):
-        RSI(df)
-        code = df.iloc[-1]['code']
-        name = df.iloc[-1]['name']
+with Pool(processes=4) as pool: # job_rsi가 main이 아니면 pool에서 오류남
+    for df, code in pool.map(get_row_for_rsi, codes['corp_code'].tolist()):
+        df = rsi(df)
+        table_last_date = table_secondary.get_last_date(code)
 
-        rsi = df[df['date'] == table.get_last_date(code).strftime('%Y-%m-%d %H:%M:%S')].iloc[-1]
+        # 테이블에 저장
+        if table_last_date is not None:  # table_last_date가 없으면 통째로 다 넣게 됨
+            df = df.drop(df[df['date'].apply(lambda x: x <= table_last_date)].index)
+        df[['date', 'code', 'name', 'rsi']].to_sql(
+            name=table_secondary.name, con=table_secondary.con, if_exists='append', index=False)
 
-        if rsi['rsi'] <= 30):
-            result = result.append(rsi, ignore_index=True)
+        # result에 저장
+        if df.iloc[-1]['rsi'] <= 30):
+            result = result.append(df.iloc[-1], ignore_index=True)
 
-        progressBar(name, codes[codes['corp_code'] == code].index[0], len(codes), bar_length=50)
+        # progress bar
+        progressBar(df.iloc[-1]['name'], codes[codes['corp_code'] == code].index[0], len(codes), bar_length=50)
 
-print(result)
-file_name = "RSI_{0}.csv".format(datetime.datetime.now().strftime("%Y-%m-%d %H시%M분"))
-result.to_csv(file_name)
-print("{0} 저장 완료".format(file_name))
 end_time = datetime.datetime.now()
+file_name = "RSI_{0}.csv".format(end_time.strftime("%Y-%m-%d %H시%M분"))
+result.sort_values(by=['volume-price', 'rsi'], ascending=False).to_csv(file_name)
+print(result.tail())
+print("{0} 저장 완료".format(file_name))
 print("종료시간: {0}".format(end_time))
 print("{0}분 소요".format(int((end_time-start_time).seconds/60)))
